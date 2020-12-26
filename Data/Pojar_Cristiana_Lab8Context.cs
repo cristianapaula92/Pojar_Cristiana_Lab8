a1c7f130439fe2bcb381af9d0a996054d88784b4
cristiana pojar <cristiana.paula92@yahoo.com>
2020-12-26 13:49:53 +0100

    Add project files.

---
+++ Data/Pojar_Cristiana_Lab8Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Pojar_Cristiana_Lab8.Models;
+
+namespace Pojar_Cristiana_Lab8.Data
+{
+    public class Pojar_Cristiana_Lab8Context : DbContext
+    {
+        public Pojar_Cristiana_Lab8Context (DbContextOptions<Pojar_Cristiana_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Pojar_Cristiana_Lab8.Models.Book> Book { get; set; }
+    }
+}
---
+++ Pages/Books/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Pojar_Cristiana_Lab8.Data;
+using Pojar_Cristiana_Lab8.Models;
+
+namespace Pojar_Cristiana_Lab8.Pages.Books
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Pojar_Cristiana_Lab8.Data.Pojar_Cristiana_Lab8Context _context;
+
+        public IndexModel(Pojar_Cristiana_Lab8.Data.Pojar_Cristiana_Lab8Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Book> Book { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Book = await _context.Book.ToListAsync();
+        }
+    }
+}
